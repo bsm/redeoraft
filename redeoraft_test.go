c9853208bea3751de9113cca1c19dce07a1204d9
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2017-06-23 10:39:10 +0100

    Increase timeout for CI

--- redeoraft_test.go
+++ redeoraft_test.go
@@ -48,7 +48,7 @@ var _ = Describe("RedeoRaft", func() {
 			s, err := readKey(cn, "key")
 			Expect(err).NotTo(HaveOccurred())
 			return s
-		}, "5s", "1s").Should(Equal(value))
+		}, "20s", "1s").Should(Equal(value))
 	}
 
 	It("should ping", func() {
@@ -188,7 +188,7 @@ var _ = Describe("RedeoRaft", func() {
 			s, err := readKey(cn2, "key")
 			Expect(err).NotTo(HaveOccurred())
 			return s
-		}, "5s").Should(Equal("v1"))
+		}, "20s").Should(Equal("v1"))
 	})
 
 	It("should propagate to all followers", func() {
